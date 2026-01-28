fffe8c56d43972f0f2a154aaaef8fb75f2a942a4
João Victor Sousa <sousa.joaovictor30@gmail.com>
2026-01-28 20:28:36 -0300

    add methods to create, update, delete, get a task, get all tasks and get tasks filtered by the state in the TodoListService class

--- src/main/java/com/joao/todo_list/service/TodoListService.java
+++ src/main/java/com/joao/todo_list/service/TodoListService.java
@@ -23,4 +23,56 @@ public class TodoListService {
         return task;
     }
 
+    public Task getTask(int id){
+        int lastId = taskList.getLast().getId();
+
+        if(id > lastId) return null;
+        Task task = null;
+
+        for(Task currTask : taskList){
+            if(currTask.getId() == lastId){
+                task = currTask;
+                break;
+            }
+        }
+        return task;
+    }
+
+    public Task update(int id, UpdateTaskRequest updateTask){
+        Task task = getTask(id);
+
+        if(task == null) return null;
+        String description = updateTask.getDescription();
+        TaskState state = updateTask.getState();
+
+        if(description != null){
+            task.setDescription(description);
+        }
+        if(state != null){
+            task.setState(state);
+        }
+        return task;
+    }
+
+    public boolean delete(int id){
+        Task task = getTask(id);
+        if(task == null) return false;
+        return taskList.remove(task);
+    }
+
+    public List<Task> getAllTasks(){
+        return taskList;
+    }
+
+    public List<Task> getTasksFilter(TaskState filter){
+        if(filter == null) return taskList;
+        List<Task> filteredList = new ArrayList<>();
+
+        for(Task task : taskList){
+            if(task.getState() == filter){
+                filteredList.add(task);
+            }
+        }
+        return filteredList;
+    }
 }
